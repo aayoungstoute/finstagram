cb6bbbd3130ed70935f769fd19e337292a14cf94
Anna Ayoung-Stoute <annastoute@gmail.com>
2023-07-12 15:22:47 +0000

    W5DAY1

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,38 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+get '/login' do    
+  erb(:login)      
+end
+
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
 
 get '/signup' do     # if a user navigates to the path "/signup",
@@ -8,6 +40,7 @@ get '/signup' do     # if a user navigates to the path "/signup",
   erb(:signup)       # render "app/views/signup.erb"
   end
 
+  
   post '/signup' do
     email      = params[:email]
     avatar_url = params[:avatar_url]
@@ -17,8 +50,9 @@ get '/signup' do     # if a user navigates to the path "/signup",
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
   
     if @user.save
-      "User #{username} saved!"
+      redirect to('/login')
     else
       erb(:signup)
     end
-  end
+  
+ end
